94f9e1d5697ef9ba196f9ad985bcf3391a0f2eb0
扛狼 <994655681@qq.com>
2022-08-08 17:07:19 +0800

    商品列表页，分页功能

--- app/goods/urls.py
+++ app/goods/urls.py
@@ -1,9 +1,10 @@
 from django.urls import re_path
-from goods.views import GoodDetailView, IndexGoodView
+from goods.views import GoodDetailView, IndexGoodView,GoodListView
 
 app_name = 'goods'
 
 urlpatterns = [
     re_path(r'^$',IndexGoodView.as_view(),name="index"),
-    re_path(r'^good_detail/(?P<good_id>[0-9]+)$',GoodDetailView.as_view(),name="detail")
+    re_path(r'^good_detail/(?P<good_id>[0-9]+)$',GoodDetailView.as_view(),name="detail"),
+    re_path(r'^list/(?P<type_id>\d+)/(?P<page>\d+)',GoodListView.as_view(),name="list") # /list/2/1?sort=default 访问商品类型为2页数为1排序为默认的商品列表数据
 ]
--- app/goods/views.py
+++ app/goods/views.py
@@ -5,6 +5,7 @@ from django.views import View
 from django.db.models import Q
 from goods.models import Goods, GoodsSKU, GoodsType, IndexCreatoryGoods,IndexGoodsBanner, IndexPromotion
 from django_redis import get_redis_connection
+from django.core.paginator import Paginator
 # Create your views here.
 
 class IndexGoodView(View):
@@ -75,3 +76,62 @@ class GoodDetailView(View):
             redis_conn.lpush('history_%d' % user_id,good_id) # 往左侧插入当前商品id记录
 
         return render(request,'detail.html',data)
+
+
+class GoodListView(View):
+
+    def get(self,request,type_id,page):
+        
+        try:
+            type = GoodsType.objects.get(id=type_id)
+        except GoodsType.DoesNotExist:
+            return redirect(reverse('goods:index'))
+        
+        # 查询所有商品分类信息
+        types = GoodsType.objects.all()
+        # 获取新品推荐数据
+        new_products = GoodsSKU.objects.filter(type=type).order_by('-create_time')[:2]
+
+        sort = request.GET.get('sort')
+        if sort == "price":
+            goods = GoodsSKU.objects.filter(type=type).order_by('price')
+        elif sort == "sales":
+            goods = GoodsSKU.objects.filter(type=type).order_by('-sales')
+        else:
+            sort = "default"
+            goods = GoodsSKU.objects.filter(type=type).order_by('-id')
+
+        good_page = Paginator(goods,1) # 1条数据为一个页码
+        try:
+            page = int(page)
+        except Exception as e:
+            page = 1
+
+        if page > good_page.num_pages:
+            page = 1
+
+        goods = good_page.page(page)
+
+        """构造页码数：返回5个页码按钮，前两个页码数，当前页码数，后两个页码数"""
+        # 当总页码数小于5时，返回所有的页码数
+        # 如果当前页是前3页，则显示1-5页
+        # 如果当前页是后3页，则显示后5页
+        # 其他情况，显示 当前页的前2页，当前页，当前页的后2页
+        if good_page.num_pages < 5:
+            page_range = range(1,good_page.num_pages+1)
+        elif goods.number <= 3:
+            page_range = range(1,6)
+        elif good_page.num_pages - goods.number <= 3:
+            page_range = range(good_page.num_pages-4,good_page.num_pages+1)
+        else:
+            page_range = range(goods.number-2,goods.number+3)
+
+        data = {"type":type,"types":types,"new_products":new_products,"goods":goods,"page_range":page_range,"sort":sort}
+
+        if request.user.is_authenticated:
+            redis_conn = get_redis_connection('default')
+            user_id = request.user.id
+            cart_len = redis_conn.hlen('cart_%d' % user_id)
+            data.update(cart_len=cart_len)
+
+        return render(request,'list.html',data)
